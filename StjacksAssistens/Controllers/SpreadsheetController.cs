654130c7e847551b372884d85fba031e0bfdd67d
darpv22-eng <darpv22@gmail.com>
2026-04-24 15:40:00 -0600

    Version 0.9(Division de planilla)

--- StjacksAssistens/Controllers/SpreadsheetController.cs
+++ StjacksAssistens/Controllers/SpreadsheetController.cs
@@ -8,22 +8,15 @@ namespace StjacksAssistens.Controllers
     public class SpreadsheetController : Controller
     {
         private readonly ApplicationDbContext _context;
-
-        // El constructor inyecta la base de datos
         public SpreadsheetController(ApplicationDbContext context)
         {
             _context = context;
         }
-
-        // GET: Spreadsheet
         public async Task<IActionResult> Index()
         {
-            // Buscamos el periodo más reciente en la base de datos
             var ultimoPeriodo = await _context.Periodss
                 .OrderByDescending(p => p.Id)
                 .FirstOrDefaultAsync();
-
-            // Creamos un objeto anónimo para pasar el ID a la vista
             var model = new
             {
                 PeriodoActualId = ultimoPeriodo?.Id ?? 0
@@ -31,14 +24,10 @@ namespace StjacksAssistens.Controllers
 
             return View(model);
         }
-
-        // GET: SpreadsheetController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
-
-        // GET: SpreadsheetController/Create
         public ActionResult Create()
         {
             return View();
@@ -57,8 +46,6 @@ namespace StjacksAssistens.Controllers
                 return View();
             }
         }
-
-        // GET: SpreadsheetController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
@@ -77,8 +64,6 @@ namespace StjacksAssistens.Controllers
                 return View();
             }
         }
-
-        // GET: SpreadsheetController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
