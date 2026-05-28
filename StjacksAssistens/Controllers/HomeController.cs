d982a8e5dc64fb15bf06f1fe62303997640fb2da
darpv22-eng <darpv22@gmail.com>
2026-05-27 23:34:11 -0600

    Version 1.9(Ausentismo)

--- StjacksAssistens/Controllers/HomeController.cs
+++ StjacksAssistens/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace StjacksAssistens.Controllers
             _logger = logger;
         }
 
+        public IActionResult Aunsentismo()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
